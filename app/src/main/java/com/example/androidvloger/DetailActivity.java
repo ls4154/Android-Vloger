4067f5c4b51de636296d8864dfd1434011bb5e0f
Youngjae Lee <steel4154@gmail.com>
2019-06-08 00:19:16 +0900

    댓글 쓰기 추가

--- app/src/main/java/com/example/androidvloger/DetailActivity.java
+++ app/src/main/java/com/example/androidvloger/DetailActivity.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -99,6 +100,16 @@ public class DetailActivity extends AppCompatActivity implements SwipeRefreshLay
 
     void onclickHeart(View view){
 
+    }
+    
+    void onClickComment(View view) {
+        if (etComment.getText().equals("")) {
+            Toast.makeText(getApplicationContext(), "Type comment first", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        
+        SendData task = new SendData();
+        task.execute("http://" + IP_ADDR + "/add_comment.php", userId, videoId, etComment.getText().toString());
     }
 
     void onclickGotoUserpage(View view){
@@ -202,5 +213,70 @@ public class DetailActivity extends AppCompatActivity implements SwipeRefreshLay
                 swipeRefreshLayout.setRefreshing(false);
             }
         },500);
+    }
+
+    class SendData extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String postParams = "iduser=" + strings[1] + "&idvideos=" + strings[2] +
+                    "&content=" + strings[3];
+
+            Log.d("COMMENT", postParams);
+            Log.d("ASYNC TASK", "DO IN BACKGROUND");
+            try {
+                URL url = new URL(strings[0]);
+                HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+                huc.setReadTimeout(5000);
+                huc.setConnectTimeout(5000);
+                huc.setRequestMethod("POST");
+                huc.connect();
+
+                OutputStream os = huc.getOutputStream();
+                os.write(postParams.getBytes("UTF-8"));
+                os.flush();
+                os.close();
+
+                int responseCode = huc.getResponseCode();
+                Log.d("response", "code:" + responseCode);
+
+                InputStream is;
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    is = huc.getInputStream();
+                } else {
+                    is = huc.getErrorStream();
+                }
+
+                InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+                BufferedReader br = new BufferedReader(isr);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+                br.close();
+                return sb.toString();
+            } catch (Exception e) {
+                return "Error "+ e.getMessage();
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if (s.substring(0, 5).equalsIgnoreCase("Error")) {
+                Toast.makeText(getApplicationContext(), "Error occured!", Toast.LENGTH_LONG).show();
+            } else {
+                // 댓글 추가됐으니 리프레시
+                etComment.setText("");
+                refresh();
+            }
+        }
     }
 }
